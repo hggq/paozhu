f32d2607acba70450554d2791505ec6a012b6176
hzq <huanguuq@163.com>
2023-05-30 08:07:43 +0800

    fix -Wall

--- vendor/httpserver/src/base64.cpp
+++ vendor/httpserver/src/base64.cpp
@@ -6,109 +6,113 @@
 namespace http
 {
 
-    // static inline bool is_base64(const char c)
-    // {
-    //     return (isalnum(c) || (c == '+') || (c == '/'));
-    // }
+// static inline bool is_base64(const char c)
+// {
+//     return (isalnum(c) || (c == '+') || (c == '/'));
+// }
 
-    std::string base64_encode(const char *bytes_to_encode, unsigned int in_len, bool isurl)
+std::string base64_encode(const char *bytes_to_encode, unsigned int in_len, unsigned char isurl)
+{
+    std::string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+    if (isurl > 1)
     {
-        std::string ret;
-        int i = 0;
-        int j = 0;
-        unsigned char char_array_3[3];
-        unsigned char char_array_4[4];
+        isurl = 1;
+    }
 
-        while (in_len--)
+    while (in_len--)
+    {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3)
         {
-            char_array_3[i++] = *(bytes_to_encode++);
-            if (i == 3)
-            {
-                char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-                char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-                char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-                char_array_4[3] = char_array_3[2] & 0x3f;
-                for (i = 0; (i < 4); i++)
-                {
-                    ret += base64_chars[isurl][char_array_4[i]];
-                }
-                i = 0;
-            }
-        }
-        if (i)
-        {
-            for (j = i; j < 3; j++)
-            {
-                char_array_3[j] = '\0';
-            }
-
             char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
             char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
             char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
             char_array_4[3] = char_array_3[2] & 0x3f;
-
-            for (j = 0; (j < i + 1); j++)
+            for (i = 0; (i < 4); i++)
             {
-                ret += base64_chars[isurl][char_array_4[j]];
+                ret += base64_chars[isurl][char_array_4[i]];
             }
-
-            while ((i++ < 3))
-            {
-                ret += '=';
-            }
+            i = 0;
         }
-        return ret;
     }
-
-    std::string base64_decode(const char *encoded_string,unsigned int in_len)
+    if (i)
     {
-        int i = 0;
-        int j = 0;
-        int in_ = 0;
-        unsigned char char_array_4[4], char_array_3[3],tempbase64=encoded_string[in_];
-        std::string ret;
-        while (in_len-- && (encoded_string[in_] != '=') &&tempbase64<128&& (base64Index[tempbase64]) > -1)
+        for (j = i; j < 3; j++)
         {
-            char_array_4[i++] = encoded_string[in_];
-            in_++;
-            if (i == 4)
-            {
-                for (i = 0; i < 4; i++)
-                {
-                    char_array_4[i] = base64Index[char_array_4[i]];
-                }
-
-                char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-                char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-                char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-                for (i = 0; (i < 3); i++)
-                    ret += char_array_3[i];
-                i = 0;
-            }
-            tempbase64=encoded_string[in_];
+            char_array_3[j] = '\0';
         }
-        if (i)
-        {
-            for (j = i; j < 4; j++)
-                char_array_4[j] = 0;
 
-            for (j = 0; j < 4; j++)
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; (j < i + 1); j++)
+        {
+            ret += base64_chars[isurl][char_array_4[j]];
+        }
+
+        while ((i++ < 3))
+        {
+            ret += '=';
+        }
+    }
+    return ret;
+}
+
+std::string base64_decode(const char *encoded_string, unsigned int in_len)
+{
+    int i   = 0;
+    int j   = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3], tempbase64 = encoded_string[in_];
+    std::string ret;
+    while (in_len-- && (encoded_string[in_] != '=') && tempbase64 < 128 && (base64Index[tempbase64]) > -1)
+    {
+        char_array_4[i++] = encoded_string[in_];
+        in_++;
+        if (i == 4)
+        {
+            for (i = 0; i < 4; i++)
             {
-                char_array_4[j] = base64Index[char_array_4[j]];
+                char_array_4[i] = base64Index[char_array_4[i]];
             }
 
             char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
             char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
             char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-            for (j = 0; (j < i - 1); j++)
-            {
-                ret += char_array_3[j];
-            }
+            for (i = 0; (i < 3); i++)
+                ret += char_array_3[i];
+            i = 0;
+        }
+        tempbase64 = encoded_string[in_];
+    }
+    if (i)
+    {
+        for (j = i; j < 4; j++)
+            char_array_4[j] = 0;
+
+        for (j = 0; j < 4; j++)
+        {
+            char_array_4[j] = base64Index[char_array_4[j]];
         }
 
-        return ret;
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+        for (j = 0; (j < i - 1); j++)
+        {
+            ret += char_array_3[j];
+        }
     }
 
+    return ret;
 }
+
+}// namespace http
