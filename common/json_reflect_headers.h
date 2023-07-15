3ddcdcac97ddf73056a97516b8c1c52872349c47
hzq <huanguuq@163.com>
2023-07-15 12:15:46 +0800

    fix create json

--- common/json_reflect_headers.h
+++ common/json_reflect_headers.h
@@ -10,102 +10,75 @@
 #include "img/upload_images.h"
 #include "unicode.h"
 
-template <typename JSON_REF_OBJ_TEMP> std::string json_encode(const JSON_REF_OBJ_TEMP &json_reflectobj)
-{
-    if (sizeof(json_reflectobj))
-    {
-    }
-    return "";
-}
+template<typename JSON_REF_OBJ_TEMP>
+std::string json_encode([[maybe_unused]] const JSON_REF_OBJ_TEMP &json_reflectobj){ return ""; }
 
-template <typename JSON_REF_OBJ_TEMP> std::string json_encode(const std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj)
-{
-    if (json_reflectobj.size() > 0)
-    {
-    }
-    return "";
-}
+template<typename JSON_REF_OBJ_TEMP>
+std::string json_encode([[maybe_unused]] const std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj){ return ""; }
 
-template <typename JSON_REF_OBJ_TEMP>
-unsigned int json_decode(JSON_REF_OBJ_TEMP &json_reflectobj, const std::string &_json_data, unsigned int _offset)
-{
-    if (_offset < _json_data.size() && sizeof(json_reflectobj))
-    {
-    }
-    return _json_data.size();
-}
+template<typename JSON_REF_OBJ_TEMP>
+unsigned int json_decode([[maybe_unused]] JSON_REF_OBJ_TEMP &json_reflectobj,[[maybe_unused]] const std::string &_json_data,[[maybe_unused]] unsigned int _offset){ return 0; }
 
-template <typename JSON_REF_OBJ_TEMP>
-unsigned int
-json_decode(std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj, const std::string &_json_data, unsigned int _offset)
-{
-    if (_offset < _json_data.size() && json_reflectobj.size() > 0)
-    {
-    }
-    return _json_data.size();
-}
+template<typename JSON_REF_OBJ_TEMP>
+unsigned int json_decode([[maybe_unused]] std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj,[[maybe_unused]] const std::string &_json_data,[[maybe_unused]] unsigned int _offset){ return 0; }
+
 
 namespace psy
 {
+
 
 std::string json_encode(const topics_tree_outjson_t &json_reflectobj);
 
+
 std::string json_encode(const std::vector<topics_tree_outjson_t> &json_reflectobj);
 
-unsigned int
-json_decode(topics_tree_outjson_t &json_reflectobj, const std::string &_json_data, unsigned int _offset = 0);
+unsigned int json_decode(topics_tree_outjson_t &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
 
-unsigned int json_decode(std::vector<topics_tree_outjson_t> &json_reflectobj,
-                         const std::string &_json_data,
-                         unsigned int _offset = 0);
-}// namespace psy
+unsigned int json_decode(std::vector<topics_tree_outjson_t> &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
+}
 
 namespace psy
 {
 
+
 std::string json_encode(const department_outjson_t &json_reflectobj);
+
 
 std::string json_encode(const std::vector<department_outjson_t> &json_reflectobj);
 
-unsigned int
-json_decode(department_outjson_t &json_reflectobj, const std::string &_json_data, unsigned int _offset = 0);
+unsigned int json_decode(department_outjson_t &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
 
-unsigned int json_decode(std::vector<department_outjson_t> &json_reflectobj,
-                         const std::string &_json_data,
-                         unsigned int _offset = 0);
+unsigned int json_decode(std::vector<department_outjson_t> &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
 
 std::string json_encode(const department_listoutjson_t &json_reflectobj);
 
+
 std::string json_encode(const std::vector<department_listoutjson_t> &json_reflectobj);
 
-unsigned int
-json_decode(department_listoutjson_t &json_reflectobj, const std::string &_json_data, unsigned int _offset = 0);
+unsigned int json_decode(department_listoutjson_t &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
 
-unsigned int json_decode(std::vector<department_listoutjson_t> &json_reflectobj,
-                         const std::string &_json_data,
-                         unsigned int _offset = 0);
-}// namespace psy
+unsigned int json_decode(std::vector<department_listoutjson_t> &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
+}
 
 namespace http
 {
 
+
 std::string json_encode(const img_upload_list_t &json_reflectobj);
+
 
 std::string json_encode(const std::vector<img_upload_list_t> &json_reflectobj);
 
-unsigned int json_decode(img_upload_list_t &json_reflectobj, const std::string &_json_data, unsigned int _offset = 0);
+unsigned int json_decode(img_upload_list_t &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
 
-unsigned int
-json_decode(std::vector<img_upload_list_t> &json_reflectobj, const std::string &_json_data, unsigned int _offset = 0);
+unsigned int json_decode(std::vector<img_upload_list_t> &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
 
 std::string json_encode(const img_upload_outjson_t &json_reflectobj);
 
+
 std::string json_encode(const std::vector<img_upload_outjson_t> &json_reflectobj);
 
-unsigned int
-json_decode(img_upload_outjson_t &json_reflectobj, const std::string &_json_data, unsigned int _offset = 0);
+unsigned int json_decode(img_upload_outjson_t &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
 
-unsigned int json_decode(std::vector<img_upload_outjson_t> &json_reflectobj,
-                         const std::string &_json_data,
-                         unsigned int _offset = 0);
-}// namespace http
+unsigned int json_decode(std::vector<img_upload_outjson_t> &json_reflectobj,const std::string &_json_data,unsigned int _offset=0);
+}
--- vendor/httpcli/typestatement.hpp
+++ vendor/httpcli/typestatement.hpp
@@ -3666,16 +3666,16 @@ unsigned int json_decode(std::vector<)";
 #include "unicode.h"
 
 template<typename JSON_REF_OBJ_TEMP>
-std::string json_encode(const JSON_REF_OBJ_TEMP &json_reflectobj){ return ""; }
+std::string json_encode([[maybe_unused]] const JSON_REF_OBJ_TEMP &json_reflectobj){ return ""; }
 
 template<typename JSON_REF_OBJ_TEMP>
-std::string json_encode(const std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj){ return ""; }
+std::string json_encode([[maybe_unused]] const std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj){ return ""; }
 
 template<typename JSON_REF_OBJ_TEMP>
-unsigned int json_decode(JSON_REF_OBJ_TEMP &json_reflectobj,const std::string &_json_data,unsigned int _offset){ return 0; }
+unsigned int json_decode([[maybe_unused]] JSON_REF_OBJ_TEMP &json_reflectobj,[[maybe_unused]] const std::string &_json_data,[[maybe_unused]] unsigned int _offset){ return 0; }
 
 template<typename JSON_REF_OBJ_TEMP>
-unsigned int json_decode(std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj,const std::string &_json_data,unsigned int _offset){ return 0; }
+unsigned int json_decode([[maybe_unused]] std::vector<JSON_REF_OBJ_TEMP> &json_reflectobj,[[maybe_unused]] const std::string &_json_data,[[maybe_unused]] unsigned int _offset){ return 0; }
 
 )";
         fwrite(&header_content[0], 1, header_content.size(), fp.get());
