6567397318cd2e1cf88093cc2769585b92051dde
hzq <huanguuq@163.com>
2023-11-14 10:40:07 +0800

    .

--- controller/src/testdownloadauth.cpp
+++ controller/src/testdownloadauth.cpp
@@ -8,6 +8,7 @@ namespace http
 std::string downloadfileauthmethod(std::shared_ptr<httppeer> peer)
 {
     httppeer &client = peer->getpeer();
+    //if (client.cookie["token"].to_string() ==md5(client.cookie["uid"].to_string()+"FWWdR@AB(755y2qwjjj)"))
     if (client.get["token"].to_int() != 123456)
     {
         client << " token is wrong ";
