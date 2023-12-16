891ffb6ec01ebd2e7c0ec84f368cc8364eae6bc4
hzq <huanguuq@163.com>
2023-12-16 21:30:37 +0800

    update version

--- vendor/httpserver/include/version.h
+++ vendor/httpserver/include/version.h
@@ -2,11 +2,11 @@
 #define __HTTP_VERSION_H
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif// defined(_MSC_VER) && (_MSC_VER >= 1200)
 
 namespace http
 {
-    #define PAOZHU_VERSION 100000
-}
+#define PAOZHU_VERSION 100502
+}// namespace http
 #endif
