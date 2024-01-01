c572cfe8f7a132c253a3266a0e4f073a00035555
hzq <huanguuq@163.com>
2024-01-01 10:40:15 +0800

    add github actions

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,7 @@
 add_rules("mode.debug", "mode.release")
 --add_requires("boost","mysql", "openssl", "libgd", "asio", "zlib", "brotli", "libqrencode", "libpng", "freetype")
 
+add_defines("release")
 
 set_languages("c++20")
 set_encodings("utf-8")
@@ -9,6 +10,7 @@ if is_plat("windows") then
     add_defines("WIN32")
     add_defines("WIN32_LEAN_AND_MEAN")
     add_syslinks("advapi32")
+    set_runtimes("MD")
     if is_mode("debug") then
         add_defines("_DISABLE_VECTOR_ANNOTATION")
         add_defines("_DISABLE_STRING_ANNOTATION")
@@ -20,88 +22,88 @@ if is_plat("mingw") then
 end
 
 add_requires("openssl","asio","zlib", "brotli")
---mysql
-if is_plat("windows") then
-    package("mysql")    
-        add_configs("shared", {description = "Download shared binaries.", default = true, type = "boolean", readonly = true})
-        set_urls("https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-$(version).tar.gz")
-        add_versions("8.0.31", "67bb8cba75b28e95c7f7948563f01fb84528fcbb1a35dba839d4ce44fe019baa")
-        add_deps("cmake")
-        on_load("windows",function(package) 
-            package:add("deps", "boost")
-            package:add("deps", "openssl")
-            package:add("deps", "zlib")
-            package:add("deps", "zstd")
-            package:add("deps", "lz4")
-        end)
+-- --mysql
+-- if is_plat("windows") then
+--     package("mysql")    
+--         add_configs("shared", {description = "Download shared binaries.", default = true, type = "boolean", readonly = true})
+--         set_urls("https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-$(version).tar.gz")
+--         add_versions("8.0.31", "67bb8cba75b28e95c7f7948563f01fb84528fcbb1a35dba839d4ce44fe019baa")
+--         add_deps("cmake")
+--         on_load("windows",function(package) 
+--             package:add("deps", "boost")
+--             package:add("deps", "openssl")
+--             package:add("deps", "zlib")
+--             package:add("deps", "zstd")
+--             package:add("deps", "lz4")
+--         end)
 
-        add_includedirs("include", "include/mysql")
+--         add_includedirs("include", "include/mysql")
 
-        on_install("mysql", function (package)
-            io.gsub("CMakeLists.txt", "ADD_SUBDIRECTORY%(storage/ndb%)", "")
-            local configs = {"-DCOMPILATION_COMMENT=XMake",
-                             "-DDEFAULT_CHARSET=utf8",
-                             "-DDEFAULT_COLLATION=utf8_general_ci",
-                             "-DINSTALL_DOCDIR=share/doc/#{name}",
-                             "-DINSTALL_INCLUDEDIR=include/mysql",
-                             "-DINSTALL_INFODIR=share/info",
-                             "-DINSTALL_MANDIR=share/man",
-                             "-DINSTALL_MYSQLSHAREDIR=share/mysql",
-                             "-DWITH_EDITLINE=bundled",
-                             "-DWITH_UNIT_TESTS=OFF",
-                             "-DDISABLE_SHARED=OFF",
-                             "-DWITH_LZ4='system'",
-                             "-DWITH_ZSTD='system'",
-                             "-DWITH_ZLIB='system'",
-                             "-DWINDOWS_RUNTIME_MD=ON",
-                             "-DWITHOUT_SERVER=ON"}
-            io.replace("cmake/ssl.cmake","IF(NOT OPENSSL_APPLINK_C)","IF(FALSE AND NOT OPENSSL_APPLINK_C)", {plain = true})
-            for _, removelib in ipairs({"icu", "libevent", "re2", "rapidjson", "protobuf", "libedit"}) do
-                io.replace("CMakeLists.txt", "MYSQL_CHECK_" .. string.upper(removelib) .. "()\n", "", {plain = true})
-                io.replace("CMakeLists.txt", "INCLUDE(" .. removelib .. ")\n", "", {plain = true})
-                io.replace("CMakeLists.txt", "WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
-                io.replace("CMakeLists.txt", "SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
-            end
-            os.rmdir("extra")
-            for _, folder in ipairs({"client", "man", "mysql-test", "libbinlogstandalone"}) do
-                os.rmdir(folder)
-                io.replace("CMakeLists.txt", "ADD_SUBDIRECTORY(" .. folder .. ")\n", "", {plain = true})
-            end
-            os.rmdir("storage/ndb")
-            for _, line in ipairs({"INCLUDE(cmake/boost.cmake)\n", "MYSQL_CHECK_EDITLINE()\n"}) do
-                io.replace("CMakeLists.txt", line, "", {plain = true})
-            end
-            io.replace("libbinlogevents/CMakeLists.txt", "INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", "MY_INCLUDE_SYSTEM_DIRECTORIES(LZ4)\nINCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", {plain = true})
-            io.replace("cmake/install_macros.cmake", "  INSTALL_DEBUG_SYMBOLS(","  # INSTALL_DEBUG_SYMBOLS(", {plain = true})
-            import("package.tools.cmake").install(package, configs)
-            if package:is_plat("windows") then
-                if package:config("shared") then
-                    os.rm(package:installdir(path.join("lib", "mysqlclient.lib")))
-                    os.cp(package:installdir(path.join("lib", "libmysql.dll")), package:installdir("bin"))
-                else
-                    os.rm(package:installdir(path.join("lib", "libmysql.lib")))
-                    os.rm(package:installdir(path.join("lib", "libmysql.dll")))
-                end
-            else
-                if package:config("shared") then
-                    os.rm(package:installdir(path.join("lib", "*.a")))
-                    os.cp(package:installdir(path.join("lib", "*.so.*")), package:installdir("bin"))
-                else
-                    os.rm(package:installdir(path.join("lib", "*.so.*")))
-                end
-            end
-        end)
+--         on_install("mysql", function (package)
+--             io.gsub("CMakeLists.txt", "ADD_SUBDIRECTORY%(storage/ndb%)", "")
+--             local configs = {"-DCOMPILATION_COMMENT=XMake",
+--                              "-DDEFAULT_CHARSET=utf8",
+--                              "-DDEFAULT_COLLATION=utf8_general_ci",
+--                              "-DINSTALL_DOCDIR=share/doc/#{name}",
+--                              "-DINSTALL_INCLUDEDIR=include/mysql",
+--                              "-DINSTALL_INFODIR=share/info",
+--                              "-DINSTALL_MANDIR=share/man",
+--                              "-DINSTALL_MYSQLSHAREDIR=share/mysql",
+--                              "-DWITH_EDITLINE=bundled",
+--                              "-DWITH_UNIT_TESTS=OFF",
+--                              "-DDISABLE_SHARED=OFF",
+--                              "-DWITH_LZ4='system'",
+--                              "-DWITH_ZSTD='system'",
+--                              "-DWITH_ZLIB='system'",
+--                              "-DWINDOWS_RUNTIME_MD=ON" .. (is_plat("windows") and package:config("vs_runtime"):startswith("MD") and "ON" or "OFF"),
+--                              "-DWITHOUT_SERVER=ON"}
+--             io.replace("cmake/ssl.cmake","IF(NOT OPENSSL_APPLINK_C)","IF(FALSE AND NOT OPENSSL_APPLINK_C)", {plain = true})
+--             for _, removelib in ipairs({"icu", "libevent", "re2", "rapidjson", "protobuf", "libedit"}) do
+--                 io.replace("CMakeLists.txt", "MYSQL_CHECK_" .. string.upper(removelib) .. "()\n", "", {plain = true})
+--                 io.replace("CMakeLists.txt", "INCLUDE(" .. removelib .. ")\n", "", {plain = true})
+--                 io.replace("CMakeLists.txt", "WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# WARN_MISSING_SYSTEM_" .. string.upper(removelib) .. "(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
+--                 io.replace("CMakeLists.txt", "SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", "# SET(" .. string.upper(removelib) .. "_WARN_GIVEN)", {plain = true})
+--             end
+--             os.rmdir("extra")
+--             for _, folder in ipairs({"client", "man", "mysql-test", "libbinlogstandalone"}) do
+--                 os.rmdir(folder)
+--                 io.replace("CMakeLists.txt", "ADD_SUBDIRECTORY(" .. folder .. ")\n", "", {plain = true})
+--             end
+--             os.rmdir("storage/ndb")
+--             for _, line in ipairs({"INCLUDE(cmake/boost.cmake)\n", "MYSQL_CHECK_EDITLINE()\n"}) do
+--                 io.replace("CMakeLists.txt", line, "", {plain = true})
+--             end
+--             io.replace("libbinlogevents/CMakeLists.txt", "INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", "MY_INCLUDE_SYSTEM_DIRECTORIES(LZ4)\nINCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/libbinlogevents/include)", {plain = true})
+--             io.replace("cmake/install_macros.cmake", "  INSTALL_DEBUG_SYMBOLS(","  # INSTALL_DEBUG_SYMBOLS(", {plain = true})
+--             import("package.tools.cmake").install(package, configs)
+--             if package:is_plat("windows") then
+--                 if package:config("shared") then
+--                     os.rm(package:installdir(path.join("lib", "mysqlclient.lib")))
+--                     os.cp(package:installdir(path.join("lib", "libmysql.dll")), package:installdir("bin"))
+--                 else
+--                     os.rm(package:installdir(path.join("lib", "libmysql.lib")))
+--                     os.rm(package:installdir(path.join("lib", "libmysql.dll")))
+--                 end
+--             else
+--                 if package:config("shared") then
+--                     os.rm(package:installdir(path.join("lib", "*.a")))
+--                     os.cp(package:installdir(path.join("lib", "*.so.*")), package:installdir("bin"))
+--                 else
+--                     os.rm(package:installdir(path.join("lib", "*.so.*")))
+--                 end
+--             end
+--         end)
 
-    package_end()    
-    add_requires("mysql");
-end
+--     package_end()    
+--     add_requires("mysql");
+-- end
 
 
 -- if !is_plat("windows") then
 --     add_requires("openssl","mysql","asio","zlib", "brotli")
 -- end   
 
-add_defines("DEBUG")
+-- add_defines("DEBUG")
 
 add_includedirs("common")
 add_includedirs("controller")
