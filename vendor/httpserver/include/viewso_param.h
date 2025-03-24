28a4a6b7bd6bd716eb3de25ca5f4a0331c8a9bac
hzq <huanguuq@163.com>
2025-03-24 21:35:21 +0800

    fix http::cookie

--- vendor/httpserver/include/viewso_param.h
+++ vendor/httpserver/include/viewso_param.h
@@ -10,13 +10,13 @@ namespace http
 {
     struct view_param
     {
-        view_param(obj_val &a,obj_val &b,cookie &c,obj_val &d):get(a),post(b),cookie(c),session(d)
+        view_param(obj_val &a,obj_val &b,http::cookie &c,obj_val &d):get(a),post(b),cookie(c),session(d)
         {
 
         }
         obj_val &get;
         obj_val &post;
-        cookie &cookie;
+        http::cookie &cookie;
         obj_val &session;
         std::string viewsopath;
     };
